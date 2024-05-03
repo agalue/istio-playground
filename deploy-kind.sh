ef47a7b7ae046a7cf6dd4f1b14768041ebdd6f1f
Alejandro Galue <galue.alejandro@gmail.com>
2024-05-03 08:51:54 -0400

    Add metrics-server helm repository

--- deploy-kind.sh
+++ deploy-kind.sh
@@ -118,6 +118,8 @@ spec:
   loadBalancerIPs: true
 EOF
 
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+helm repo update
 helm upgrade --install metrics-server metrics-server/metrics-server \
  -n kube-system --set args={--kubelet-insecure-tls}
 
