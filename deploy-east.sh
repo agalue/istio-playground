7024dfaf3e7bdcd667033e01271c288b806360af
Alejandro Galue <galue.alejandro@gmail.com>
2025-05-03 10:37:06 -0400

    Ensure native routing on Cilium.

--- deploy-east.sh
+++ deploy-east.sh
@@ -8,7 +8,7 @@ SUBNET=${SUBNET-248} # For Cilium L2/LB (must be unique across all clusters)
 WORKERS=${WORKERS-1}
 CLUSTER_ID=${CLUSTER_ID-1} # Unique on each cluster
 POD_CIDR=${POD_CIDR-10.11.0.0/16} # Must be under 10.0.0.0/8 for Cilium ipv4NativeRoutingCIDR
-SVC_CIDR=${SVC_CIDR-172.21.0.0/16} # Node that Kind Docker Network is 172.18.0.0/16 by default (worker nodes)
+SVC_CIDR=${SVC_CIDR-172.21.0.0/16} # Must differ from Kind's Docker Network
 
 echo "Deploying Kubernetes"
 . deploy-kind.sh
